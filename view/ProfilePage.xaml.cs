baa07b1c13d8bed5744293a551929bfab7031bc3
a-szulc <104166505+a-szulc@users.noreply.github.com>
2024-06-17 13:59:59 +0200

    added delete button in profile page

--- view/ProfilePage.xaml.cs
+++ view/ProfilePage.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InfiWheelDesktop.services;
+using InfiWheelDesktop.viewModel;
+using System.Net.Http;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using InfiWheelDesktop.viewModel;
 
 namespace InfiWheelDesktop
 {
@@ -30,6 +20,40 @@ namespace InfiWheelDesktop
         private void GoBackButton_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new CarListPage());
+        }
+
+        private void OrderHistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+        private async void DeleteAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            var messageBoxResult = MessageBox.Show($"Do you really want to delete your account forewer? (This is a really long time)", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Information);
+
+            if (messageBoxResult != MessageBoxResult.Yes) return;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string token = TokenManager.Instance.GetToken();
+                    client.BaseAddress = new Uri("https://infiwheel.azurewebsites.net");
+                    client.Timeout = TimeSpan.FromSeconds(Convert.ToDouble(1000000));
+
+                    HttpResponseMessage response = new HttpResponseMessage();
+                    string userId = await RentCarService.GetUserIdFromToken(token);
+
+                    response = await client.DeleteAsync($"/User/delete/{userId}");
+                    response.EnsureSuccessStatusCode();
+
+                    this.NavigationService.Navigate(new LoginPage());
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show("Error deleting the account, try again later");
+                return;
+            }
+
         }
     }
 }
